528013e73727d128b2467845d8669319e62fec69
Pedro <mendez.pedro94@gmail.com>
2021-06-20 00:30:12 -0400

    desafio2 listo

--- desafio2.sql
+++ desafio2.sql
@@ -1,18 +1,19 @@
 -- Requerimientos Iniciales
 
-sudo su - dvaldes
-
-psql
-
-dvaldes=# CREATE DATABASE clase02;
-
+--sudo su - pedro --> para ir a la carpeta inicial
+-->psql
+\c postgres
+DROP DATABASE desafio2;
+CREATE DATABASE desafio2;
+\c desafio2
+-- desactivamos el autocommit
 \echo :AUTOCOMMIT
 \set AUTOCOMMIT off
 
 
 -- Solicitud 01
-
-psql -U dvaldes clase02 < unidad2.sql
+--comando ingresado en la terminal (fuera del psql)
+psql -U pedro desafio2 < unidad2.sql
 
 -- Solicitud 02
 BEGIN TRANSACTION;
@@ -34,17 +35,29 @@ INSERT INTO detalle_compra (producto_id,compra_id,cantidad) VALUES (8,(SELECT ma
 UPDATE producto SET stock = stock - 3 WHERE id=1;
 UPDATE producto SET stock = stock - 3 WHERE id=2;
 UPDATE producto SET stock = stock - 3 WHERE id=8;
+COMMIT;
 SELECT * FROM producto WHERE id = 1;
 SELECT * FROM producto WHERE id = 2;
 SELECT * FROM producto WHERE id = 8;
-COMMIT;
+
 
 --solicitud 4
-
+--desabilitar autocommit
 \echo :AUTOCOMMIT
 \set AUTOCOMMIT off
-
+--insertat un nuevo cliente
 BEGIN TRANSACTION;
-INSER INTO cliente (nombre,email) VALUES (usuario011, usuario011@hotmaicl.com);
+savepoint nueva_cuenta;
+-- no se puede modificar por el usuario?
+INSERT INTO cliente (id,nombre,email) VALUES (11,usuario011, usuario011@hotmail.com);
+-- reviso si agrego otro usuario
 SELECT max(id) from cliente;
-COMMIT
+--realizo un rollback
+rollback to nueva_cuenta;
+COMMIT;
+-- confirmo que la informacion no fue modificada
+select * from cliente;
+
+-- activamos el autocommit
+\set AUTOCOMMIT on
+\echo :AUTOCOMMIT
